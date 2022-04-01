c9934d64b07585e8b9db8a8b765b9164daad2791
Yoann GOURDON <64698244+YoannGourdon@users.noreply.github.com>
2022-04-01 10:46:07 +0200

    Add files via upload
    API avec GET, POST, PUT et DELETE fonctionnels

--- TP_ESEO_API_Rest_Serveur/src/main/java/com/controller/VilleController.java
+++ TP_ESEO_API_Rest_Serveur/src/main/java/com/controller/VilleController.java
@@ -1,5 +1,6 @@
 package com.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.ui.Model;
@@ -17,22 +18,67 @@ import com.dto.Ville;
 public class VilleController {
 
 	// fonction pour récupérer le contenu de la BDD
-	@RequestMapping(value="/ville", method=RequestMethod.GET)
-	
-	public @ResponseBody Iterable<Ville> get(@RequestParam(required  = false, value="codePostal") String codePostal) {
-		
+	@RequestMapping(value = "/ville", method = RequestMethod.GET)
+
+	public @ResponseBody Iterable<Ville> get(@RequestParam(required = false, value = "codePostal") String codePostal) {
 		System.out.println("get");
 		// TODO : mon code vers la BDD
-		List<Ville> liste = VilleDAO.recupererVilles();
+		List<Ville> liste = new ArrayList<Ville>();
+		if (codePostal == null) {
+			liste = VilleDAO.recupererVilles();
+		} else {
+			if (codePostal.equals("")) {
+				liste = VilleDAO.recupererVilles();
+			} else {
+				liste = VilleDAO.recupererVilleCode(codePostal);
+			}
+
+		}
+
 		return liste;
 	}
-	
-	// TODO : 
+
+	// TODO :
 	// fonction pour enregistrer un element dans la BDD
-	@RequestMapping(value="/ville", method=RequestMethod.POST)
+	@RequestMapping(value = "/ville", method = RequestMethod.POST)
 	@ResponseBody
 	public String insert(@RequestBody Ville ville, Model model) {
-		
-		return "todo";
+		String resultat = "";
+		if (ville != null) {
+			resultat = VilleDAO.AjouterVille(ville);
+		}
+
+		return resultat;
 	}
+
+	// TODO :
+	// fonction pour modifier un element dans la BDD
+	@RequestMapping(value = "/ville", method = RequestMethod.PUT)
+	@ResponseBody
+	public String update(@RequestBody Ville ville, Model model) {
+		String resultat = "";
+		if (ville != null) {
+			if(VilleDAO.villePresente(ville)) {
+				resultat = VilleDAO.ModifierVille(ville);
+			}else {
+				resultat = VilleDAO.AjouterVille(ville);
+			}
+		}
+
+		return resultat;
+	}
+
+	// TODO :
+	// fonction pour supprimer un element de la BDD
+	@RequestMapping(value = "/ville", method = RequestMethod.DELETE)
+	@ResponseBody
+	public String delete(@RequestParam(required = true, value = "code") String code) {
+		String resultat = "";
+		if (code != null) {
+			resultat = VilleDAO.SupprimerVilleAvecCode(code);
+		}
+
+		return resultat;
+	}
+
 }
